6a3fb26d0ad00b095016f6ab412c72e8b4db2cb2
Omar Zuniga <miguel_omar30@hotmail.com>
2025-09-19 19:32:12 -0600

    Background Jobs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { TRPCReactProvider } from "@/trpc/client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -26,7 +27,10 @@ export default function RootLayout({
 	return (
 		<TRPCReactProvider>
 			<html lang="en">
-				<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+				<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+					{children}
+					<Toaster richColors />
+				</body>
 			</html>
 		</TRPCReactProvider>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
 const Page = () => {
+	const trpc = useTRPC();
+	const invoke = useMutation(
+		trpc.invoke.mutationOptions({
+			onSuccess: () => {
+				toast.success("Invoked successfully");
+			},
+		})
+	);
+
 	return (
-		<div>
-			<h1>Hello World</h1>
+		<div className="p-4 max-w-7xl mx-auto">
+			<Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "John" })}>
+				Invoke
+			</Button>
 		</div>
 	);
 };
